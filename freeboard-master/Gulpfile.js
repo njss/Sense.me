3e9697cfdfae831fe6decd128329d4bb98baf812
Nelson Silva <nelson.silva@vc.fraunhofer.at>
2015-11-06 23:09:14 +0100

    cleaned

--- freeboard-master/Gulpfile.js
+++ freeboard-master/Gulpfile.js
@@ -1,15 +1,38 @@
 var gulp = require('gulp');
+// var styleguide = require('sc5-styleguide');
+// var outputPath = 'styleguideoutput';
+// var sass = require('gulp-sass');
 //,
         //sass = require('gulp-ruby-sass'),
         //autoprefixer = require('gulp-autoprefixer'),
         //minifycss = require('gulp-minify-css'),
         //rename = require('gulp-rename');
 
+// gulp.task('styleguide:generate', function() {
+//   return gulp.src('app/*.html')
+//     .pipe(styleguide.generate({
+//         title: 'My Styleguide',
+//         server: true,
+//         rootPath: outputPath,
+//         overviewPath: 'README.md'
+//       }))
+//     .pipe(gulp.dest(outputPath));
+// });
+
+// gulp.task('styleguide:applystyles', function() {
+//   return gulp.src('main.scss')
+//     .pipe(sass({
+//       errLogToConsole: true
+//     }))
+//     .pipe(styleguide.applyStyles())
+//     .pipe(gulp.dest(outputPath));
+// });        
+
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
   app.use(require('connect-livereload')({port: 35729}));
-  app.use(express.static(__dirname));
+  app.use(express.static(__dirname + '/app'));
   app.listen(4000, '0.0.0.0');
 });
 
@@ -20,7 +43,7 @@ gulp.task('livereload', function() {
 });
 
 function notifyLiveReload(event) {
-  var fileName = require('path').relative(__dirname, event.path);
+  var fileName = require('path').relative(__dirname + '/app', event.path);
 
   tinylr.changed({
     body: {
@@ -38,14 +61,26 @@ gulp.task('styles', function() {
 });
 */
 
+// gulp.task('watch', ['styleguide'], function() {
+//   //gulp.watch('sass/*.scss', ['styles']);
+//   gulp.watch(['app/**/*.html'], ['styleguide'], notifyLiveReload);
+//   gulp.watch(['app/css/*'], ['styleguide'], notifyLiveReload);
+//   gulp.watch(['app/**/*.js'], ['styleguide'], notifyLiveReload);
+// });
+
 gulp.task('watch', function() {
   //gulp.watch('sass/*.scss', ['styles']);
-  gulp.watch('app/*.html', notifyLiveReload);
-  gulp.watch('app/*.css', notifyLiveReload);
-  gulp.watch('app/*.js', notifyLiveReload);
+  gulp.watch(['app/**/*.html'], notifyLiveReload);
+  gulp.watch(['app/css/*'], notifyLiveReload);
+  gulp.watch(['app/**/*.js'], notifyLiveReload);
 });
 
 //gulp.task('default', ['styles', 'express', 'livereload', 'watch'], function() {
-gulp.task('default', ['express', 'livereload', 'watch'], function() {
+// gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
+// gulp.task('default', ['express', 'livereload', 'watch'], function() {
+
+// });
+
+gulp.task('default', ['express', 'watch'], function() {
 
 });
